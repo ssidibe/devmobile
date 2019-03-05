d5b698884d0998646c95df37e10e070b4e3ff6e1
Samba SIDIBE <contact@sambasidibe.com>
2019-03-05 03:31:01 +0000

    toto

---
+++ Toto/app/src/main/java/thread/teach/sambasidibe/com/toto/MainActivity.java
@@ -0,0 +1,13 @@
+package thread.teach.sambasidibe.com.toto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
